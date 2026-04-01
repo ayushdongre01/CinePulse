47b12f8ecc7479653a44d0a9e36180630971177c
ayushdongre01 <ayushdongre69@gmail.com>
2026-04-01 17:58:19 +0530

    Added backend link

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 
 # ─── CONFIG ───────────────────────────────────────────────────────────────────
-API_BASE  = "http://127.0.0.1:8000"
+API_BASE  = "https://cinepulse-6e4g.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG  = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(
